b9d34275981efd52b7a43f250308fdaeb835b4c5
朽木不可雕也 <aszswaz@163.com>
2022-03-27 23:49:55 +0800

    backup

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -1,17 +1,21 @@
-(custom-set-variables
- '(package-selected-packages '(atom-one-dark-theme ##)))
-(custom-set-faces
- ;; 设置字体
- '(default ((t (:family "JetBrains Mono" :foundry "JB" :slant normal :weight normal :height 120 :width normal)))))
+;; 设置字体
+(set-frame-font "Source Han Sans CN 13" nil t)
 
-;; 窗口最大化
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
 ;; 设置禁用欢迎页
 (setq inhibit-splash-screen t)
 ;; 禁用备份
 (setq make-backup-files nil)
 ;; 禁用自动保存
 (setq auto-save-default nil)
+;; 关闭启动画面
+(setq inhibit-startup-message t)
+;; 高亮当前行
+(global-hl-line-mode 1)
+;; 隐藏工具栏、菜单栏、导航条
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+;; 设置窗口大小
 
 ;; 添加软件包列表，网站首页：https://melpa.org/#/getting-started
 (require 'package)
