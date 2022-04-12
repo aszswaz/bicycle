25267f747ceee295063cdf42ec474007c83f2edd
朽木不可雕也 <aszswaz@163.com>
2022-04-12 15:41:07 +0800

    backup

--- shell/init.zsh
+++ shell/init.zsh
@@ -11,6 +11,6 @@ source "$(dirname $0)/alias/index.zsh"
 
 # 设置文件夹背景色
 eval "$(dircolors ./dir_colors)"
-[ ! -e "${HOME}/.dir_colors" ] && ln -s ./dir_colors ~/.dir_colors
+[ ! -e "${HOME}/.dir_colors" ] && ln -s "$(pwd)/dir_colors" "${HOME}/.dir_colors"
 
 cd "$WORK_DIR"
